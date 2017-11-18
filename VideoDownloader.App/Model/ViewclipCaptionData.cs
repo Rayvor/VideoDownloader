01123c6023275615718edc517079b28b74affd71
Andrey Kuksenkov <sp1ne2104@gmail.com>
2017-11-18 16:08:35 +0300

    Create ViewclipCaptionData.cs

---
+++ VideoDownloader.App/Model/ViewclipCaptionData.cs
@@ -0,0 +1,19 @@
+using Newtonsoft.Json;
+
+namespace VideoDownloader.App.Model
+{
+    class ViewclipCaptionData
+    {
+        [JsonProperty("a")]
+        public string Author { get; set; }
+
+        [JsonProperty("cn")]
+        public int ClipIndex { get; set; }
+
+        [JsonProperty("lc")]
+        public string Locale { get; set; }
+
+        [JsonProperty("m")]
+        public string ModuleName { get; set; }
+    }
+}
